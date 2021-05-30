e1843da1189de48b7950479bd83971f86a4c8bfd
Kalin Roussinov <kalinrouss@gmail.com>
2021-05-30 15:45:52 +0300

    working on the ingridient list and Recipe List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import RecipeList from './RecipeList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecipeList recipes={sampleRecipes} />
   );
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '0:45',
+    instructions: '1. Put Salt and Pepper. \n2. Put the chicken in the oven. \n3. Eat the chicken.',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        amount: '2 Pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Teaspoon'
+      }
+    ]
+  },
+  {
+    id: 1,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '1:45',
+    instructions: '1. Put Salt, Pepper and Paprika. \n2. Put the pork in the oven. \n3. Eat the pork.',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '2 Pounds'
+      },
+      {
+        id: 2,
+        name: 'Paprika',
+        amount: '2 Teaspoon'
+      }
+    ]
+  }
+]
 
 export default App;
